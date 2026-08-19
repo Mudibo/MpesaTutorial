d688b601a5986b190b4a13645b556669691ef17d
Mudibo <sean.mudibo@strathmore.edu>
2026-08-19 23:31:24 +0300

    (feat): Added connection to supabase database

--- src/config/env.js
+++ src/config/env.js
@@ -20,4 +20,8 @@ export const config = {
     database: {
         url: process.env.DATABASE_URL,
     },
+    supabase: {
+        url: process.env.SUPABASE_URL,
+        serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    },
 };
