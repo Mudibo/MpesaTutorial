78e3f3cb307ccd6606ea1d2edf1d103eae85020f
Mudibo <sean.mudibo@strathmore.edu>
2026-08-20 12:29:29 +0300

    (feat); Added payment repo file and payment status constant

---
+++ src/constants/payment-status.js
@@ -0,0 +1,9 @@
+export const PAYMENT_STATUS = Object.freeze({
+    PENDING: "pending",
+    STK_SENT: "stk_sent",
+    WAITING_CALLBACK: "waiting_callback",
+    SUCCESS: "success",
+    FAILED: "failed",
+    CANCELLED: "cancelled",
+    EXPIRED: "expired",
+});
---
+++ src/repositories/payment.repository.js
@@ -0,0 +1,63 @@
+import {supabase} from "../config/supabase.js";
+import { PAYMENT_STATUS } from "../constants/payment-status.js";
+
+
+class PaymentRepository{
+    async create(payment){
+        const {data, error} = await supabase.from("payments").insert(payment).select().single();
+        if(error){
+            throw error;
+        }
+        return data;
+    }
+    async findByReference(reference){
+        const {data, error} = await supabase.from("payments").select("*").eq("reference", reference).single();
+        if(error){
+            throw error;
+        }
+        return data;
+    }
+    async findByCheckoutRequestId(checkoutRequestId){
+        const {data, error} = await supabase.from("payments").select("*").eq("checkout_request_id", checkoutRequestId).single();
+        if(error){
+            throw error;
+        }
+        return data;
+    }
+    async updateSTKResponse(reference, merchantRequestId, checkoutRequestId){
+        const {data, error} = await supabase.from("payments").update({
+            merchant_request_id: merchantRequestId,
+            checkout_request_id: checkoutRequestId,
+            status: PAYMENT_STATUS.WAITING_CALLBACK,
+        }).eq("reference", reference).select().single();
+        if(error){
+            throw error;
+        }
+        return data;
+    }
+    async markSuccessfulPayment(checkoutRequestId, receiptNumber,resultCode, resultDescription){
+        const {data, error} = await supabase.from("payments").update({
+            status: PAYMENT_STATUS.SUCCESS,
+            mpesa_receipt_number: receiptNumber,
+            result_code: resultCode,
+            result_description: resultDescription,
+        }).eq("checkout_request_id", checkoutRequestId).select().single();
+        if(error){
+            throw error;
+        }
+        return data;
+    }
+    async markFailedPayment(checkoutRequestId, resultCode, resultDescription){
+        const {data, error} = await supabase.from("payments").update({
+            status: PAYMENT_STATUS.FAILED,
+            result_code: resultCode,
+            result_description: resultDescription,
+        }).eq("checkout_request_id", checkoutRequestId).select().single();
+        if(error){
+            throw error;
+        }
+        return data;
+    }
+}
+
+export default new PaymentRepository();
